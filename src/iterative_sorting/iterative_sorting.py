c9f71ccdd9aa23019db506f197e398147843bd81
Michael Mangus <mjmangus16@yahoo.com>
2019-12-11 12:00:50 -0500

    completed MVP

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,11 +1,11 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+# TO-DO: Complete the selection_sort() function below
+def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
+        # (hint, can do in 3 loc)
 
         print(arr[cur_index])
 
@@ -13,38 +13,33 @@ def selection_sort( arr ):
             if arr[smallest_index] > arr[y]:
                 smallest_index = y
 
-        arr[smallest_index],arr[cur_index] = arr[cur_index],arr[smallest_index]
-
+        arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
 
         # TO-DO: swap
-
-
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+def bubble_sort(arr):
 
     swap = True
 
     while swap == True:
-        change = False
-
-        for i in range(0, len(arr) -1):
+        swap = False
+        for i in range(0, len(arr) - 1):
             if arr[i] > arr[i+1]:
-                arr[i],arr[i+1] = arr[i+1],arr[i]
-                change = True
-        
-        if change == False:
-            swap = False
-  
-    print(arr)
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                swap = True
+
     return arr
 
 
+bubble_sort([4, 3, 1, 2, 6, 9, 7, 8, 5])
+
+
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+def count_sort(arr, maximum=-1):
     output = []
 
     if len(arr) == 0:
@@ -55,7 +50,7 @@ def count_sort( arr, maximum=-1 ):
 
     if mini < 0:
         return "Error, negative numbers not allowed in Count Sort"
-    
+
     count = [0 for i in range(mini, maxi)]
     print(count)
 
@@ -67,13 +62,8 @@ def count_sort( arr, maximum=-1 ):
             for x in range(y):
                 output.append(index + mini)
 
-    
-
     print(arr)
     print(count)
     print(output)
 
     return output
-    
-
-count_sort([1,2,3,6,5,4,2,4,5,4,4])
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,17 +1,46 @@
 # TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
+def merge(arrA, arrB):
+    elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
+
+    for i in range(elements):
+        print("test", arrA, arrB, "index", i)
+        if len(arrA) > 0 and len(arrB) > 0:
+            if arrA[0] > arrB[0]:
+                merged_arr[i] = arrB[0]
+                arrB.pop(0)
+            else:
+                merged_arr[i] = arrA[0]
+                arrA.pop(0)
+        elif len(arrA):
+            merged_arr[i] = arrA[0]
+            arrA.pop(0)
+        elif len(arrB):
+            merged_arr[i] = arrB[0]
+            arrB.pop(0)
+    print(merged_arr)
+
     # TO-DO
-    
+
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
+def merge_sort(arr):
+
     # TO-DO
+    half = len(arr) // 2
+
+    if len(arr) > 1:
+        arr = merge(merge_sort(arr[half:]), merge_sort(arr[:half]))
 
     return arr
+
+
+cont1 = [1, 3, 5, 6, 7, 9, 2, 4, 8]
+
+cont = merge_sort(cont1)
+print(cont)
 
 
 # STRETCH: implement an in-place merge sort algorithm
@@ -20,7 +49,8 @@ def merge_in_place(arr, start, mid, end):
 
     return arr
 
-def merge_sort_in_place(arr, l, r): 
+
+def merge_sort_in_place(arr, l, r):
     # TO-DO
 
     return arr
@@ -28,6 +58,6 @@ def merge_sort_in_place(arr, l, r):
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
+def timsort(arr):
 
     return arr
